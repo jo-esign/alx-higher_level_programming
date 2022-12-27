4d39bfcbd08d5a8c44dc8692bff4b02f49587cb9
Jo <69627271+jo-esign@users.noreply.github.com>
2022-12-27 16:42:06 +0100

    done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
-
-
 def best_score(a_dictionary):
-    """
-    gets the best value from a dictionary (greatest integer)
-    """
-    max_value = 0
-    winner = None
-    if type(a_dictionary) is dict:
-        for key, value in a_dictionary.items():
-            if value > max_val:
-                max_val = value
-                winner = key
-    return winner
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
